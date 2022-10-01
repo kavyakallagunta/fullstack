1c05fc6fd828e5eebedf6f21cb5c9ae0f5928898
kavyakallagunta <94386680+kavyakallagunta@users.noreply.github.com>
2022-10-01 14:18:49 +0530

    Add files via upload

---
+++ nodejsexamples/add.js
@@ -0,0 +1,27 @@
+// console.log("kim taehyung");
+// function add(a,b){
+//     return a+b
+// }
+// console.log(add(1,3));
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// function sub(a,b){
+//     console.log(a-b);
+// }
+// module.exports=add;
+// module.exports=sub;
+
+function add(a,b){
+    console.log(a+b);
+}
+function sub(a,b){
+    console.log(a-b);
+}
+module.exports={
+    add,
+    sub
+}
+
+
---
+++ nodejsexamples/first.js
@@ -0,0 +1,7 @@
+// console.log("kim");
+// require('./add')
+// const a = require('./add')
+// a(5,1);
+const a = require('./add')
+a.add(5,1);
+a.sub(5,1);
